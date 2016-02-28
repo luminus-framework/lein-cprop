b4f838341f95127ffae1dc8f79af10761fa71a50
Yogthos <dmitri.sotnikov@gmail.com>
2016-02-27 23:35:16 -0500

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject lein-cprop "1.0.0"
+  :description "Leiningen plugin for cprop"
+  :url "https://github.com/tolitius/cprop"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :eval-in-leiningen true)
---
+++ src/lein_cprop/plugin.clj
@@ -0,0 +1,21 @@
+(ns lein-cprop.plugin
+  (:use [robert.hooke :only (add-hook)])
+  (:require [clojure.java.io :as io]
+            leiningen.core.main))
+
+(defn- as-edn [& args]
+  (binding [*print-dup*    false
+            *print-meta*   false
+            *print-length* false
+            *print-level*  false]
+    (apply prn-str args)))
+
+(defn env-file [project]
+  (io/file (:root project) "user.edn"))
+
+(defn- write-env-to-file [func task-name project args]
+  (spit (env-file project) (as-edn (:env project {})))
+  (func task-name project args))
+
+(defn hooks []
+  (add-hook #'leiningen.core.main/apply-task #'write-env-to-file))
